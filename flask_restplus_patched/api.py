c9ecd4db1565a556efa5fdb942b4d53e39ebef30
Jeremy Epstein <jazepstein@gmail.com>
2017-10-15 10:13:49 +1100

    initial commit

---
+++ flask_restplus_patched/api.py
@@ -0,0 +1,34 @@
+from flask import jsonify
+from flask_restplus import Api as OriginalApi
+from werkzeug import cached_property
+
+from ._http import HTTPStatus
+from .namespace import Namespace
+from .swagger import Swagger
+
+
+class Api(OriginalApi):
+
+    @cached_property
+    def __schema__(self):
+        # The only purpose of this method is to pass custom Swagger class
+        return Swagger(self).as_dict()
+
+    def init_app(self, app):
+        super(Api, self).init_app(app)
+        app.errorhandler(HTTPStatus.UNPROCESSABLE_ENTITY.value)(handle_validation_error)
+
+    def namespace(self, *args, **kwargs):
+        # The only purpose of this method is to pass a custom Namespace class
+        _namespace = Namespace(*args, **kwargs)
+        self.add_namespace(_namespace)
+        return _namespace
+
+
+# Return validation errors as JSON
+def handle_validation_error(err):
+    exc = err.data['exc']
+    return jsonify({
+        'status': HTTPStatus.UNPROCESSABLE_ENTITY.value,
+        'message': exc.messages
+    }), HTTPStatus.UNPROCESSABLE_ENTITY.value
---
+++ setup.py
@@ -0,0 +1,41 @@
+import os
+
+import setuptools
+
+module_path = os.path.join(os.path.dirname(__file__), 'flask_restplus_patched/__init__.py')
+version_line = [line for line in open(module_path)
+                if line.startswith('__version__')][0]
+
+__version__ = version_line.split('__version__ = ')[-1][1:][:-2]
+
+setuptools.setup(
+    name="flask-restplus-patched",
+    version=__version__,
+    url="https://github.com/Jaza/flask-restplus-patched",
+
+    author="Jeremy Epstein",
+    author_email="jazepstein@gmail.com",
+
+    description="Extends Flask-RESTplus so it can handle Marshmallow schemas and Webargs arguments.",
+    long_description=open('README.md').read(),
+
+    py_modules=['flask-restplus-patched'],
+    zip_safe=False,
+    platforms='any',
+
+    install_requires=[
+        'Flask', 'flask-restplus', 'marshmallow', 'flask-marshmallow',
+        'webargs', 'apispec'],
+
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
+    ],
+)
