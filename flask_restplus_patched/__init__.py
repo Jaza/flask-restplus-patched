ccabc5920677efa964936a16122d44ecad71fbec
Jeremy Epstein <Jeremy.Epstein@iress.com.au>
2018-05-10 20:11:07 +1000

    fix modelschema import for non sqlalchemy setup

--- flask_restplus_patched/__init__.py
+++ flask_restplus_patched/__init__.py
@@ -1,9 +1,14 @@
-__version__ = '0.1.3'
+__version__ = '0.1.4'
 
 from flask_restplus import *
 from .api import Api
-from .model import Schema, ModelSchema, DefaultHTTPErrorSchema
+from .model import Schema, DefaultHTTPErrorSchema
 from .namespace import Namespace
 from .parameters import Parameters, PostFormParameters, PatchJSONParameters
 from .swagger import Swagger
 from .resource import Resource
+
+try:
+    from .model import ModelSchema
+except ImportError:
+    pass
